c866ed84f1903a594a793192e80a08ea2cfed5a3
GabrielBentoG <gabrielbentog0@gmail.com>
2023-10-05 17:24:49 -0300

    refactor home page

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,14 +1,39 @@
 import React from "react";
 import './HomePage.css'
 import Cards from '../../components/Cards/Cards'
+import Transactions from '../../components/Transactions/Transactions'
+import Winnings from '../../components/Winnings/Winnings'
+import Budget from "../../components/Budget/Budget";
+import Subscriptions from "../../components/Subscriptions/Subscriptions";
+import Savings from "../../components/Savings/Savings";
+import Loans from "../../components/Loans/Loans";
+import Financial from "../../components/Financial/Financial";
 const HomePage = () => {
     return (
-        <div className="main-content-holder">
-            <div className="content-grid-one">
-                <Cards/>
+      <div className="main-content-holder">
+          <div className="content-grid-one">
+              <Cards />
+              <Transactions />
+              <Winnings />
+          </div>
+          <div className="content-grid-two">
+            <Budget />
+            <div className="grid-two-item">
+              <div className="subgrid-two">
+                <Subscriptions />
+                <Savings />
+              </div>
+            </div>
+
+            <div className="grid-two-item">
+              <div className="subgrid-two">
+                <Loans />
+                <Financial />
+              </div>
             </div>
         </div>
-    );
-};
-
-export default HomePage;
+    </div>
+    )
+  }
+  
+  export default HomePage
